5b8099b282e2719e80bbdd9f5d013943f13363c5
AVKrivenko <krivenkonastyaline4@mail.ru>
2024-12-02 00:16:29 +0300

    rel

--- tests/test_main.py
+++ tests/test_main.py
@@ -7,3 +7,6 @@ def test_add():
 def test_subtract():
     assert subtract(5, 3) == 2
     assert subtract(1, -1) == 2
+def test_multiply():
+    assert multiply(2, 3) == 6
+    assert multiply(-1, 1) == -1
