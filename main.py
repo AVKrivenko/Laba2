38535f28f2abee4b9efcdbab86c59c929595e69b
AVKrivenko <131263658+AVKrivenko@users.noreply.github.com>
2024-12-01 23:44:56 +0300

    dev

--- main.py
+++ main.py
@@ -3,7 +3,10 @@ def add(a, b):
 
 def subtract(a, b):
     return a - b
-
+    
+def test_multiply():
+    assert multiply(2, 3) == 6
+    assert multiply(-1, 1) == -1
 if __name__ == "__main__":
     print("Addition:", add(5, 3))
     print("Subtraction:", subtract(5, 3))
