0be37e2a427fd6554a19bc0cb906ca9f3da476bc
AVKrivenko <131263658+AVKrivenko@users.noreply.github.com>
2024-12-01 23:46:13 +0300

    Update  dev main.py

--- main.py
+++ main.py
@@ -4,9 +4,8 @@ def add(a, b):
 def subtract(a, b):
     return a - b
     
-def test_multiply():
-    assert multiply(2, 3) == 6
-    assert multiply(-1, 1) == -1
+def multiply(a, b):
+    return a * b
 if __name__ == "__main__":
     print("Addition:", add(5, 3))
     print("Subtraction:", subtract(5, 3))
